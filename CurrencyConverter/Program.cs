040982a12833fe074f0bf46fe627b674dde3bce6
Matyasmester <matyas.zoboki@gmail.com>
2021-07-27 17:59:09 +0200

    Add change default command, refactor code

--- CurrencyConverter/Program.cs
+++ CurrencyConverter/Program.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text.Json;
+using System.IO;
 using static CurrencyConverter.CommandError;
 
 namespace CurrencyConverter
 {
     class Program
     {
-        static string currencyFrom = "eur";
-        static string currencyTo = "huf";
-
+        static string defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "defaults.txt");
         const string link = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/currFrom/currTo.json";
+
+        static string[] defaultValues = GetDefaultValues();
+
+        static string currencyFrom = defaultValues[0];
+        static string currencyTo = defaultValues[1];
+
         private static void WriteColoredString(ConsoleColor color, string msg)
         {
             Console.ForegroundColor = color;
@@ -25,16 +30,29 @@ namespace CurrencyConverter
             return first.Length == 3 && second.Length == 3;
         }
 
+        private static string[] GetDefaultValues()
+        {
+            if (!File.Exists(defaultPath)) return new[] { "eur", "huf" };
+            return File.ReadAllLines(defaultPath);
+        }
+
         private static CommandError ExecuteCommand(string[] args)
         {
+            string first = string.Empty; 
+            string second = string.Empty;
             string command = args[0];
+
+            if(command == "cc" || command == "cd")
+            {
+                first = args[1];
+                second = args[2];
+            }
+
             switch (command)
             {
                 // Change Currency
                 case "cc":
                     if (args.Length != 3) return SyntaxError;
-                    string first = args[1];
-                    string second = args[2];
                     if (!IsValidCurrencyPair(first, second)) return InvalidCurrency;
                     currencyFrom = first;
                     currencyTo = second;
@@ -45,10 +63,17 @@ namespace CurrencyConverter
                     currencyFrom = currencyTo;
                     currencyTo = temp;
                     return OK;
+                // Change Defaults
+                case "cd":
+                    if (args.Length != 3) return SyntaxError;
+                    if (!IsValidCurrencyPair(first, second)) return InvalidCurrency;
+                    File.WriteAllLines(defaultPath, new[] { first, second });
+                    return OK;
                 case "?":
                 case "help":
                     WriteColoredString(ConsoleColor.Green, "cc = Change Currency | usage: cc [currFrom] [currTo] | e.g: cc btc eur\n" +
                                                            "swap | Swaps the two currencies\n" +
+                                                           "cd = Change Defaults | usage: cd [currency] [currency] | e.g: cd usd eur\n" +
                                                            "General: Type a number and it will convert it to the other currency.\n");
                     return OK;
                 default:
@@ -65,7 +90,7 @@ namespace CurrencyConverter
                 WriteColoredString(ConsoleColor.Yellow, $"[{currencyFrom} --> {currencyTo}] : ");
                 string[] args = Console.ReadLine().TrimEnd(' ').Split(' ');
 
-                double input = 0.0;
+                double input;
                 bool isNum = double.TryParse(args[0], out input);
                 if (!isNum)
                 {
@@ -88,9 +113,10 @@ namespace CurrencyConverter
                 
                 string finalURL = link.Replace("currFrom", currencyFrom).Replace("currTo", currencyTo);
 
+                string json = string.Empty;
+
                 using (WebClient wc = new WebClient())
                 {
-                    string json = string.Empty;
                     try
                     {
                         json = wc.DownloadString(finalURL);
@@ -101,17 +127,17 @@ namespace CurrencyConverter
                         WriteColoredString(ConsoleColor.Red, $"Web Service returned {response.StatusCode}, probably non-existent currency names. \n");
                         continue;
                     }
-                    JsonDocument document = JsonDocument.Parse(json);
-
-                    JsonElement root = document.RootElement;
-
-                    JsonElement valElement = root.GetProperty(currencyTo);
-                    double converted = valElement.GetDouble() * input;
-
-                    WriteColoredString(ConsoleColor.Cyan, $"{input} {currencyFrom} = {converted} {currencyTo} \n");
-
-                    document.Dispose();
                 }
+                JsonDocument document = JsonDocument.Parse(json);
+
+                JsonElement root = document.RootElement;
+
+                JsonElement valElement = root.GetProperty(currencyTo);
+                double converted = valElement.GetDouble() * input;
+
+                WriteColoredString(ConsoleColor.Cyan, $"{input} {currencyFrom} = {converted} {currencyTo} \n");
+
+                document.Dispose();
             }
         }
     }
